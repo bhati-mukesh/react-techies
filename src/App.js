63622219a1ecc9af86ee5a56bfc89222a5a40719
Mukesh <mukeshbhati24@gmail.com>
2020-08-29 15:56:17 +0530

    added navbar and linked to empty pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/navbar/Navbar.component'
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from './pages/components/home/Home.component'
+import About from './pages/components/about/About.component'
+import Services from './pages/components/services/Services.component'
+import Contact from './pages/components/contact/Contact.component'
 
 const App= () => {
   return (
+    <>
     <div className="App">
+    <Navbar />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/contact' component={Contact} />
+      <Route exact path='/services' component={Services} />
+      <Redirect to='/' />
+    </Switch>
     </div>
+    </>
   );
 }
 
