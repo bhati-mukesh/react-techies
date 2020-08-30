f2a727f346bd55df64a3dacfec36551dd11acec2
Mukesh <mukeshbhati24@gmail.com>
2020-08-30 07:24:48 +0530

    Updates

---
+++ precache-manifest.3d3e20c9eb72a9ed98a3fb95d920709f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3528a68b1dbe3c1403ff7cc76f3a89de",
+    "url": "/react-techies/index.html"
+  },
+  {
+    "revision": "628d493a46938cb60a51",
+    "url": "/react-techies/static/css/main.d0051570.chunk.css"
+  },
+  {
+    "revision": "22e7629a22f7d3bc6a5e",
+    "url": "/react-techies/static/js/2.c775544d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-techies/static/js/2.c775544d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628d493a46938cb60a51",
+    "url": "/react-techies/static/js/main.016e9be4.chunk.js"
+  },
+  {
+    "revision": "3ba46337fd73a76c1098",
+    "url": "/react-techies/static/js/runtime-main.e7536dd8.js"
+  },
+  {
+    "revision": "59844546e4c8a4a61f4ba1a54b3392e8",
+    "url": "/react-techies/static/media/android.59844546.jpg"
+  },
+  {
+    "revision": "e65751d1a8bad22de2a682e905439012",
+    "url": "/react-techies/static/media/app.e65751d1.png"
+  },
+  {
+    "revision": "598ec0cb94456c05ae8e3661f38e9b92",
+    "url": "/react-techies/static/media/digimarketing.598ec0cb.jpeg"
+  },
+  {
+    "revision": "7736253843ca673932727ea8bd97d9a8",
+    "url": "/react-techies/static/media/home.77362538.jpg"
+  },
+  {
+    "revision": "5c3a5200655d7b8fbd3593ddce3a1290",
+    "url": "/react-techies/static/media/home3.5c3a5200.jpg"
+  },
+  {
+    "revision": "7a2f37073b6cdc0892d3906be2e9f851",
+    "url": "/react-techies/static/media/marketing.7a2f3707.jpg"
+  },
+  {
+    "revision": "c0c9c06c76b44120a2ebff1599e8d318",
+    "url": "/react-techies/static/media/software.c0c9c06c.png"
+  }
+]);
